d3e1c16eba7310aa9db9fd01827338efa94c21ca
Eliseu Videira <eliseuvideira1@gmail.com>
2021-06-26 07:26:39 -0300

    :sparkles: added create find one unit test

---
+++ packages/model/test/functions/createFindOne.test.ts
@@ -0,0 +1,41 @@
+import { createFindOne } from "../../src/functions/createFindOne";
+
+interface PackageProps {
+  name: string;
+  version: string;
+}
+
+describe("createFindOne", () => {
+  it("creates a find one method", async () => {
+    expect.assertions(7);
+
+    const table = "packages";
+
+    const findOne = createFindOne<PackageProps>({ table });
+
+    const item = { id: Math.random() };
+
+    const first = jest.fn(() => item);
+    const andWhere = jest.fn();
+    const builder = { andWhere };
+    const modify = jest.fn((modify) => {
+      modify(builder);
+
+      return { first };
+    });
+    const from = jest.fn(() => ({ modify }));
+    const database = { from } as any;
+
+    const name = "react";
+
+    const value = await findOne({ database, filter: { $eq: { name } } });
+
+    expect(value).toEqual(item);
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(modify).toHaveBeenCalledTimes(1);
+    expect(andWhere).toHaveBeenCalledTimes(1);
+    expect(andWhere).toHaveBeenCalledWith("name", "=", name);
+    expect(from).toHaveBeenCalledTimes(1);
+    expect(from).toHaveBeenCalledWith(table);
+  });
+});
